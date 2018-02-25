1003f36634bb0d9e16ae6b91a3c8ed8a4be717c7
tca19 <30314448+tca19@users.noreply.github.com>
2018-02-25 18:56:23 +0100

    optimize code (sequence length memoization)

--- problem_014.c
+++ problem_014.c
@@ -1,25 +1,42 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
 int32_t main()
 {
-	/* for each integer i under 1M, compute the length of its Collatz
-	 * sequence.  Only save the integer with the longest sequence. */
-	uint64_t longest = 0, answer, n, i, s;
+	/* Compute the length of Collatz sequence for each integer under 1M. Save
+	 * the sequence lengths for fast retrieval - so no need to recompute the
+	 * whole sequence everytime. */
+	uint64_t N = 1000000, longest = 0, answer, n, i, s;
+	uint64_t *lengths = (uint64_t*) calloc(N, sizeof(uint64_t));
+	for (i = 0; i < N; ++i)
+		lengths[i] = 0;
 
-	for (i = 1; i < 1000001; ++i)
+	for (i = 1; i < N+1; ++i)
 	{
-		n = i; /* need a copy because we are going to modify it. */
+		n = i; /* need a copy because we are going to modify it */
 		s = 1; /* initial sequence length */
+
 		while (n > 1)
 		{
+			/* look if we already know the length of sequence
+			 * starting at n */
+			if (n < N && lengths[n-1] > 0)
+			{
+				s += lengths[n-1] - 1;
+				break;
+			}
+
+			/* else, compute new term and increment sequence length */
 			if (n&1)
 				n = 3*n + 1;
 			else
 				n >>= 1;
 			++s;
 		}
+
+		lengths[i-1] = s;
 
 		if (s > longest)
 		{
@@ -29,5 +46,6 @@ int32_t main()
 	}
 
 	printf("Problem 14: %" PRIu64 "\n", answer);
+	free(lengths);
 	return 0;
 }
