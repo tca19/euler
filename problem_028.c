fa00735c0dd39087c0cb4b64b8686a82f8ffc447
tca19 <30314448+tca19@users.noreply.github.com>
2018-03-05 22:36:46 +0100

    add explanations for problem 28

--- problem_028.c
+++ problem_028.c
@@ -4,13 +4,18 @@
 
 int32_t main()
 {
+	/* We need to add the 4 numbers in the corner of each square. If the
+	 * square has a side of length n, then top-rigth corner is n^2, top-left
+	 * corner is n^2-(n-1), bottom-left corner is n^2-2(n-1) and
+	 * bottom-right corner is n^2-3(n-1). So when we increase the spiral
+	 * size with 1 square, we add 4n^2 - 6(n-1) to the answer. */
 	uint64_t answer, n;
 
-	answer = 1;
-	for (n = 3; n <= 1001; n += 2)
-	{
-		answer += 4*n*n - 6*(n-1);
-	}
+	answer = 1; /* center of grid, need to be added to the sum. */
+
+	/* Size increases by 2 at each new square, until we reach a size of 1001
+	 * by 1001. */
+	for (n = 3; n < 1002; n += 2) answer += 4*n*n - 6*(n-1);
 
 	printf("Problem 28: %" PRIu64 "\n", answer);
 	return 0;
