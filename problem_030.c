9a31edff240b8737405b37c0e3448ca62dbec7c9
tca19 <30314448+tca19@users.noreply.github.com>
2018-03-09 22:56:36 +0100

    add explanation for code optimization

--- problem_030.c
+++ problem_030.c
@@ -6,7 +6,15 @@ int32_t main()
 {
 	uint64_t answer = 0, sum_powers, i, n, d;
 
-	for (i = 2; i < 354294; ++i)
+	/* We are looking for numbers equal to the sum of the fifth power of its
+	 * digits. We need to find the upper bound of these numbers (when we can
+	 * stop because next numbers can not validate the rule.
+	 *
+	 * If the number has 6 digits, then the greatest sum of powers we can
+	 * possibly compute is 354294 (6 * 9^5). So every numbers n > 354294 can
+	 * not validate the rule (no equality is possible since the sum will
+	 * always be lower than n). So we can stop at 354294. */
+	for (i = 2; i < 354295; ++i)
 	{
 		n = i; /* copy because we need to modify it */
 		sum_powers = 0;
