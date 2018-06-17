38354aeaed76d7c623ec8bc0a108c753a92136bf
tca19 <30314448+tca19@users.noreply.github.com>
2018-06-17 09:39:00 +0200

    optimize solution of problem 45

--- problem_026-050/problem_045.c
+++ problem_026-050/problem_045.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
-#define SIZE 200000
-
-int32_t contains(int64_t *ar, int64_t v, int32_t size)
+/* is_pentagonal: return 1 if N is a pentagonal number, else 0 */
+int64_t is_pentagonal(int64_t N)
 {
-	while (size-- > 0)
-		if (*ar++ == v)
-			return 1;
-	return 0;
+	/* If N is a pentagonal number, there exists an integer n such that
+	 * N = P(n) = (3n^2 - n) / 2
+	 *
+	 * So n is a solution of the equation 3n^2 - n - 2N = 0.
+	 * We have D = (-1)^2 - 4(3 * (-2N)) = 24N + 1
+	 * Two solutions:
+	 *   - X1 = (1 - (sqrt(24N+1) ) / 6
+	 *   - X2 = (1 + (sqrt(24N+1) ) / 6
+	 *
+	 * Only X2 is a positive solution. So if N is a pentagonal number, the
+	 * solution X2 must be an integer. */
+	double val = (sqrt(24*N+1) + 1) / 6;
+	return val == (int) val;
 }
 
 int32_t main(void)
 {
-	int64_t triangular[SIZE], pentagonal[SIZE], hexagonal[SIZE];
-	int32_t i;
+	/* Hexagonal numbers are also triangular numbers, so only need to find a
+	 * number which is both hexagonal and pentagonal. */
 
-	for (i = 1; i < SIZE; ++i)
-		triangular[i] = i * (i+1) / 2;
-	for (i = 1; i < SIZE; ++i)
-		pentagonal[i] = i * (3*i - 1) / 2;
-	for (i = 1; i < SIZE; ++i)
-		hexagonal[i] = i * (2*i - 1);
+	int64_t i = 144; /* example given is H_143, need to find the next one */
+	while (!is_pentagonal(i * (2*i - 1)))
+		++i;
 
-	for (i = 1; i < SIZE; ++i)
-	{
-		if (contains(pentagonal, hexagonal[i], SIZE))
-			printf("%ld\n", hexagonal[i]);
-	}
+	printf("Problem 45: %ld\n", i * (2*i - 1));
 
 	return 0;
 }
