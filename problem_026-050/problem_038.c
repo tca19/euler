2a812ef0f1e0d36fc9b83a64feb667ac4a40cf6c
tca19 <30314448+tca19@users.noreply.github.com>
2018-06-20 23:52:40 +0200

    add explanations for problem 38

--- problem_026-050/problem_038.c
+++ problem_026-050/problem_038.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/* is_pandigital: return 1 if n is pandigital (use all digits 1-9 once) */
 int32_t is_pandigital(int64_t n)
 {
+	/* count occurrence of each digit. n is pandigital if all digits have 1
+	 * occurrence and there is no 0 */
 	int8_t digits[10] = {0};
 
 	while (n > 0)
@@ -18,12 +21,18 @@ int32_t is_pandigital(int64_t n)
 	return digits[0] == 0;
 }
 
+/* concatenate_products: compute and concatenate the products 1*n, 2*n, 3*n...
+ * until it forms at least a 9-digits number */
 int64_t concatenate_products(int64_t n)
 {
 	int64_t i, d, product = n;
 
 	for (i = 2; product <= 100000000; ++i)
 	{
+		/*  concatenate two integers is non trivial so mutiply current
+		 *  concatenated products by 10 for each digit in d (e.g. by
+		 *  1000 if there is 3 digits in d), then add d, same as
+		 *  concatenating d. */
 		d = i * n;
 		while (d > 0)
 		{
