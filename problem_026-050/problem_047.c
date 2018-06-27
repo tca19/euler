c76b8a313e5c3a6f54c1784a128aab97a4be21ba
tca19 <30314448+tca19@users.noreply.github.com>
2018-06-27 22:35:35 +0200

    optimize solution for problem 47

--- problem_026-050/problem_047.c
+++ problem_026-050/problem_047.c
@@ -3,41 +3,26 @@
 
 #define N 1000000
 
-uint8_t sieve[N];
+uint8_t number_prime_factor[N];
 
-/* init_sieve: initialize the array to test if a number is prime. */
-void init_sieve(void)
+/* init_NPF: initialize the Number of Prime Factor (NPF) array. */
+void init_NPF(void)
 {
 	uint32_t i, j;
 
-	/* at first, every integer is considered to be prime, except 0 and 1 */
+	/* similar to Eratosthene sieve. Initialize all numbers with 0 prime
+	 * factors counter. Goes from 2 to N. If the current number has 0 prime
+	 * factor, it means it's a prime, so increment the counter for all
+	 * multiples of this prime (e.g. 2 is prime so every even number has
+	 * at least 2 as a prime factor).
+	 */
 	for (i = 0; i < N; ++i)
-		sieve[i] = 1;
-	sieve[0] = sieve[1] = 0;
+		number_prime_factor[i] = 0;
 
 	for (i = 2; i < N; ++i)
-		if (sieve[i]) /* prime, mark all its multiples as non prime */
+		if (number_prime_factor[i] == 0)
 			for (j = 2*i; j < N; j += i)
-				sieve[j] = 0;
-}
-
-/* number_prime_factor: return the number of different prime factor of n */
-int32_t number_prime_factor(int64_t n)
-{
-	int32_t i = n, n_factor = 0;
-
-	while (i > 1)
-	{
-		if (sieve[i] && n % i == 0)
-		{
-			++n_factor;
-			while (n % i == 0)
-				n /= i;
-		}
-		--i;
-	}
-
-	return n_factor;
+				++number_prime_factor[j];
 }
 
 /* find the first 4 consecutive integers that have 4 different prime factors */
@@ -45,21 +30,20 @@ int32_t main(void)
 {
 	int32_t i;
 
-	init_sieve();
+	init_NPF();
 	for (i = 0; i < N-4; ++i)
 	{
-		if (number_prime_factor(i) != 4)
+		if (number_prime_factor[i] != 4)
 			continue;
-		if (number_prime_factor(i+1) != 4)
+		if (number_prime_factor[i+1] != 4)
 			continue;
-		if (number_prime_factor(i+2) != 4)
+		if (number_prime_factor[i+2] != 4)
 			continue;
-		if (number_prime_factor(i+3) != 4)
+		if (number_prime_factor[i+3] != 4)
 			continue;
 		break;
 	}
 
 	printf("Problem 47: %d\n", i);
-
 	return 0;
 }
