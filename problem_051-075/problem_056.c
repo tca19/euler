28a9ff7794224336b07fff7f2b9f1d7468be4df2
tca19 <30314448+tca19@users.noreply.github.com>
2018-07-04 10:39:48 +0200

    optimize solution 56 (sum digits when multiply)

--- problem_051-075/problem_056.c
+++ problem_051-075/problem_056.c
@@ -1,52 +1,49 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* multiply: multiply n by m where each digit of n is stored in a different cell
- * of digits. n is stored in reverse order (1234 is stored as [4,3,2,1]). */
-void multiply(uint8_t *digits, int32_t len, int32_t m)
+/* 100^100 has 200 digits. Since a^b < 100^100 for a,b in 1..100, the maximum
+ * number of digits in a^b is 200 */
+#define LEN 200
+
+/* multiply_and_sum: multiply n by m where each digit of n is stored in a
+ * different cell of array digits[]. n is stored in reverse order (1234 is
+ * stored as [4,3,2,1]). Compute the sum of all digits as it processes the array
+ */
+int32_t multiply_and_sum(int32_t *digits, int32_t m)
 {
-	int32_t i, carry;
+	int32_t i, carry, sum;
 
-	carry = 0;
-	for (i = 0; i < len; ++i)
+	carry = sum = 0;
+	for (i = 0; i < LEN; ++i)
 	{
 		carry = digits[i] * m + carry;
 		digits[i] = carry%10;
+		sum += digits[i];
 		carry /= 10;
 	}
-}
 
-/* sum: return the sum of all cells of ar */
-int32_t sum(uint8_t *ar, int32_t len)
-{
-	int32_t result = 0;
-
-	while (--len >= 0)
-		result += ar[len];
-	return result;
+	return sum;
 }
 
 /* sum the digits of each product a^b, print the largest sum */
 int32_t main(void)
 {
-	uint8_t *digits;
-	int32_t a, b, tmp, max_sum;
+	int32_t a, b, tmp, max_sum, *digits;
 
-	digits  = calloc(200, sizeof *digits);
+	digits = calloc(LEN, sizeof *digits);
 	max_sum = 0;
 
-	for (a = 1; a < 100; ++a)
+	for (a = 2; a < 100; ++a)
 	{
 		/* reset array */
-		for (tmp = 0; tmp < 200; ++tmp)
-			digits[tmp] = 0;
+		memset(digits, 0, LEN * sizeof *digits);
 		digits[0] = 1;
 
 		for (b = 1; b < 100; ++b)
 		{
-			multiply(digits, 200, a);
-			tmp = sum(digits, 200);
+			tmp = multiply_and_sum(digits, a);
 			if (tmp > max_sum)
 				max_sum = tmp;
 		}
