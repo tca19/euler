a455bfdd2db09deef4b165e1f035d0c9373240ea
tca19 <30314448+tca19@users.noreply.github.com>
2018-07-05 23:36:34 +0200

    refactor solution 55 (function to add n+reverse(n))

--- problem_051-075/problem_055.c
+++ problem_051-075/problem_055.c
@@ -2,8 +2,8 @@
 #include <stdint.h>
 #include <string.h>
 
-#define LEN 200
-#define N 10000
+#define SIZE 40  /* 50 iterations will use numbers with at most 40 digits */
+#define N 10000  /* count the Lychrel numbers from 1 to 10'000 */
 
 /* is_palindrom: return 1 if array ar is palindromic, 0 otherwise */
 int32_t is_palindrom(uint8_t *ar, int32_t len)
@@ -15,13 +15,35 @@ int32_t is_palindrom(uint8_t *ar, int32_t len)
 	return 1;
 }
 
+/* sum_reverse: compute n + reverse(n) where digits of n are stored in ar[].
+ * Store result in sum[]. Return the number of digits in sum[] */
+int32_t sum_reverse(uint8_t *ar, uint8_t *sum, int32_t len)
+{
+	int32_t carry, i;
+
+	for (i = 0, carry = 0; i < len; ++i)
+	{
+		carry += ar[i] + ar[len-1 - i];
+		sum[i] = carry%10;
+		carry /= 10;
+	}
+
+	while (carry > 0)
+	{
+		sum[i++] = carry%10;
+		carry /= 10;
+	}
+
+	return i;
+}
+
 /* is_lychrel: return 1 if n is a Lychrel number (summing n and reverse(n) gives
  * a palindromic number) or n+reverse(n) is a Lychrel number. Return 0 otherwise
  * (not a Lychrel number, even after 50 iterations). */
 int32_t is_lychrel(int32_t n)
 {
-	uint8_t digits[LEN] = {0}, sum[LEN] = {0};
-	int32_t carry, iteration, i, j;
+	uint8_t digits[SIZE] = {0}, sum[SIZE] = {0};
+	int32_t n_iter, i;
 
 	/* store digits of n into an array */
 	for (i = 0; n > 0; ++i)
@@ -30,46 +52,15 @@ int32_t is_lychrel(int32_t n)
 		n /= 10;
 	}
 
-	/* compute n+reverse(n) */
-	for (j = 0, carry = 0; j != i; j++)
+	for (n_iter = 0; n_iter < 50; ++n_iter)
 	{
-		carry += digits[j] + digits[i-1-j];
-		sum[j] = carry%10;
-		carry /= 10;
-	}
-	while (carry > 0)
-	{
-		sum[j++] = carry%10;
-		carry /= 10;
-	}
+		i = sum_reverse(digits, sum, i);
 
-	if (is_palindrom(sum, j))
-		return 1;
-
-	iteration = 1;
-	while (iteration < 51)
-	{
-		memcpy(digits, sum, LEN * sizeof(uint8_t));
-		memset(sum, 0, LEN * sizeof(uint8_t));
-		i = j;
-
-		/* compute n+reverse(n) */
-		for (j = 0, carry = 0; j != i; j++)
-		{
-			carry += digits[j] + digits[i-1-j];
-			sum[j] = carry%10;
-			carry /= 10;
-		}
-		while (carry > 0)
-		{
-			sum[j++] = carry%10;
-			carry /= 10;
-		}
-
-		if (is_palindrom(sum, j))
+		if (is_palindrom(sum, i))
 			return 1;
 
-		++iteration;
+		memcpy(digits, sum, i * sizeof(uint8_t));
+		memset(sum, 0, i * sizeof(uint8_t));
 	}
 
 	return 0;
