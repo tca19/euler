afa7a8080fbf531c088fd777fbda153fcb0a64cd
tca19 <30314448+tca19@users.noreply.github.com>
2018-07-07 22:41:33 +0200

    add explanations for problem 57

--- problem_051-075/problem_057.c
+++ problem_051-075/problem_057.c
@@ -6,6 +6,8 @@
 /* at most 400 digits in numerator or denominator */
 #define LEN 400
 
+/* add: add b to a. Both are represented as an array of digits, in reverse order
+ * (123 is [3, 2, 1]) */
 void add(uint8_t *a, uint8_t *b)
 {
 	int32_t i, carry;
@@ -18,15 +20,19 @@ void add(uint8_t *a, uint8_t *b)
 	}
 }
 
+/* n_digits: count the number of digits in a (array of digits) */
 int32_t n_digits(uint8_t *a)
 {
 	int32_t i;
 
-	for (i = 0; a[LEN-1 - i] == 0; ++i)
+	/* find the right-most digit that is non zero */
+	for (i = LEN-1; a[i] == 0 && i > 0; --i)
 		;
-	return LEN - i;
+	return i;
 }
 
+/* compute the 1000 first expansions of sqrt(2). Find the number of time the
+ * numerator has more digits than the denominator */
 int32_t main(void)
 {
 	uint8_t *num, *den, *tmp;;
@@ -36,6 +42,7 @@ int32_t main(void)
 	den = calloc(LEN, sizeof *den);
 	tmp = calloc(LEN, sizeof *tmp);
 
+	/* 3/2 is the first expansion of sqrt(2), given in problem statement */
 	num[0] = 3;
 	den[0] = 2;
 	iter = 1;
@@ -43,6 +50,14 @@ int32_t main(void)
 
 	while (iter++ < 1000)
 	{
+		/* at step n:
+		 *   num/den
+		 *
+		 * at step n+1:
+		 *   1 + 1/(num/den + 1) = 1 + 1/((num+den)/den)
+		 *                       = 1 + den/(num+den)
+		 *                       = (num+den+den) / (num+den)
+		 */
 		memcpy(tmp, num, LEN * sizeof *tmp);
 		add(num, den);
 		add(num, den);
