8af14e76dab14cd54e461d3a61e509a061be911d
tca19 <30314448+tca19@users.noreply.github.com>
2018-07-11 23:58:49 +0200

    start to automate the process of finding key

--- problem_051-075/problem_059.c
+++ problem_051-075/problem_059.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define KEYSIZE 3
 
 int32_t *text;
 int32_t size;
 
-/* read_ciphered_text: read the ciphered text in `filename` into the global
+/* read_ciphered_text: read the ciphered text from `filename` into the global
  * variable `̣text`. Set the global variable `size` with the text length */
 void read_ciphered_text(char *filename)
 {
@@ -19,7 +22,9 @@ void read_ciphered_text(char *filename)
 		size = 0;
 	}
 
-	/* start by counting the number of char to allocate enough memory */
+	/* start by counting the number of chars to allocate enough memory. The
+	 * file is composed of ascii values separated by commas, so count the
+	 * number of commas to get the number of ascii values */
 	size = 0;
 	while ((c = fgetc(fp)) != EOF)
 		if (c == ',')
@@ -41,7 +46,7 @@ void decipher(int32_t *deciphered_text, char *key, int32_t key_size)
 		deciphered_text[i] = text[i] ^ key[i%key_size];
 }
 
-int32_t count_occurrence(int32_t *s, int32_t len, int32_t *sub, int32_t sub_len)
+int32_t count_occurrence(int32_t *s, int32_t len, char *sub, int32_t sub_len)
 {
 	int32_t i, j, count = 0;
 
@@ -66,39 +71,42 @@ void print_deciphered(int32_t *deciphered_text)
 
 int32_t main(void)
 {
-	int32_t KEYSIZE = 3;
-	char key[KEYSIZE+1];
-	int32_t *deciphered_text;
-	int32_t a, b, c;
-	int32_t sub[] = {'t', 'h', 'e'};
-	int32_t answer = 0;
+	char key[KEYSIZE], best_key[KEYSIZE];
+	char the[] = {'t', 'h', 'e'};
+	int32_t i, j, k, n, *deciphered_text, max_count_occ, answer;
 
 	read_ciphered_text("problem_051-075/p059_cipher.txt");
 	deciphered_text = calloc(size, sizeof *deciphered_text);
 
-	key[3] = '\0';
-	for (a = 0; a < 26; ++a)
-		for (b = 0; b < 26; ++b)
-			for (c = 0; c < 26; ++c)
+	answer = max_count_occ = 0;
+	for (i = 0; i < 26; ++i)
+	{
+		key[0] = 'a' + i;
+		for (j = 0; j < 26; ++j)
+		{
+			key[1] = 'a' + j;
+			for (k = 0; k < 26; ++k)
 			{
-				key[0] = 'a' + a;
-				key[1] = 'a' + b;
-				key[2] = 'a' + c;
+				key[2] = 'a' + k;
 				decipher(deciphered_text, key, KEYSIZE);
-				if (count_occurrence(deciphered_text, size, sub, 3) > 3)
+				n = count_occurrence(deciphered_text, size, the, 3);
+				if (n > max_count_occ)
 				{
-					printf("%s %d\n", key,
-					count_occurrence(deciphered_text, size, sub, 3));
+					printf("ok with %d\n", max_count_occ);
+					memcpy(best_key, key, KEYSIZE);
+					max_count_occ = n;
 				}
 			}
+		}
+	}
 
 
-	decipher(deciphered_text, "god", KEYSIZE);
-	print_deciphered(deciphered_text);
+	printf("%s\n", key);
 
-	for (a = 0; a < size; ++a)
-		answer += deciphered_text[a];
+	for (i = 0; i < size; ++i)
+		answer += deciphered_text[i];
 
+				if (count_occurrence(deciphered_text, size, the, 3) > 3)
 	printf("Problem 59: %d\n", answer);
 	return 0;
 }
