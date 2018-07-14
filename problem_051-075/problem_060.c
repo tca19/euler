c1f06452a82bfb2175c45a00efa59007bcce1eec
tca19 <30314448+tca19@users.noreply.github.com>
2018-07-14 22:48:55 +0200

    optimize solution for problem 60
    Instead of having a large array (1M) to test for primality, uses a
    smaller one (10K) and test primality by looking if there is no prime
    divider less than sqrt(n)

--- problem_051-075/problem_060.c
+++ problem_051-075/problem_060.c
@@ -1,48 +1,54 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
+#include <stdlib.h>   /* calloc */
+#include <string.h>   /* memset */
 
-#define MAX 100000000
 #define SIZE 10000
 
-int8_t sieve[MAX];
+int8_t sieve[SIZE];
 
 void init_sieve(void)
 {
 	int32_t i, j;
 
-	for (i = 0; i < MAX; ++i)
-		sieve[i] = 1;
+	memset(sieve, 1, SIZE * sizeof sieve[0]);
 	sieve[0] = sieve[1] = 0;
 
-	for (i = 0; i < MAX; ++i)
+	for (i = 2; i < SIZE; ++i)
 		if (sieve[i])
-			for (j = 2*i; j < MAX; j += i)
+			for (j = 2*i; j < SIZE; j += i)
 				sieve[j] = 0;
+}
+
+int32_t is_prime(int32_t n)
+{
+	int32_t i;
+
+	if (n%2 == 0)
+		return 0;
+
+	if (n < SIZE)
+		return sieve[n];
+
+	for (i = 3; i*i <= n; i += 2)
+		if (sieve[i] && n%i == 0)
+			return 0;
+
+	return 1;
 }
 
 int32_t concatenate(int32_t n, int32_t m)
 {
-	int32_t reverse_m = 0;
+	int32_t i, copy_m;
 
-	while (m > 0)
-	{
-		reverse_m = 10*reverse_m + m%10;
-		m /= 10;
-	}
-
-	while (reverse_m > 0)
-	{
-		n = 10*n + reverse_m%10;
-		reverse_m /= 10;
-	}
-
-	return n;
+	for (i = 1, copy_m = m; copy_m > 0; i *= 10)
+		copy_m /= 10;
+	return n*i + m;
 }
 
 int32_t is_prime_pair(int32_t p1, int32_t p2)
 {
-	return sieve[concatenate(p1, p2)] && sieve[concatenate(p2, p1)];
+	return is_prime(concatenate(p1, p2)) && is_prime(concatenate(p2, p1));
 }
 
 int32_t main(void)
@@ -55,16 +61,15 @@ int32_t main(void)
 
 	init_sieve();
 
-	/* initialize graph, edges represent prime pairs */
 	pairs = calloc(SIZE*SIZE, sizeof *pairs);
 	for (i = 0; i < SIZE; ++i)
 	{
-		if (!sieve[i])
+		if (!is_prime(i))
 			continue;
 
 		for (j = i+1; j < SIZE; ++j)
 		{
-			if (!sieve[j])
+			if (!is_prime(j))
 				continue;
 
 			if (is_prime_pair(i, j))
@@ -75,11 +80,10 @@ int32_t main(void)
 		}
 	}
 
-	/* find clique of size 5 */
 	answer = 5*SIZE;
 	for (a = 0; a < SIZE; ++a)
 	{
-		if (!sieve[a])
+		if (!is_prime(a))
 			continue;
 
 		for (b = a+1; b < SIZE; ++b)
