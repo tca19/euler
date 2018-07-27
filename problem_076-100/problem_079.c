2748242c8a3a945065c08a914e13c4dda2a7822a
tca19 <30314448+tca19@users.noreply.github.com>
2018-07-27 22:32:56 +0200

    solve problem 79

--- problem_076-100/problem_079.c
+++ problem_076-100/problem_079.c
@@ -3,11 +3,36 @@
 #include <stdlib.h>
 
 #define FILENAME "problem_076-100/p079_keylog.txt"
+#define LEN 10
 
+int32_t index_score_max(int8_t *scores, int8_t *occurrences)
+{
+	int32_t index, i;
+	float max_scores;
+
+	index = -1;
+	max_scores = 0.0;
+	for (i = 0; i < LEN; ++i)
+	{
+		if (occurrences[i] == 0)
+			continue;
+
+		if ((float) scores[i] / occurrences[i] > max_scores)
+		{
+			index = i;
+			max_scores = (float) scores[i] / occurrences[i];
+		}
+	}
+
+	if (index != -1)
+		scores[index] = -1;
+	return index;
+}
 int32_t main(void)
 {
 	FILE *fp;
-	int32_t val;
+	int8_t scores[LEN] = {0}, occurrences[LEN] = {0};
+	int32_t val, answer;
 
 	if ((fp = fopen(FILENAME, "r")) == NULL)
 	{
@@ -16,7 +41,21 @@ int32_t main(void)
 	}
 
 	while (fscanf(fp, "%d", &val) != EOF)
-		printf("%d\n", val);
+	{
+		scores[val/100] += 3;
+		++occurrences[val/100];
 
+		scores[val%100 / 10] += 2;
+		++occurrences[val%100 / 10];
+
+		scores[val%10] += 1;
+		++occurrences[val%10];
+	}
+
+	answer = 0;
+	while ((val = index_score_max(scores, occurrences)) != -1)
+		answer = answer * 10 + val;
+
+	printf("Problem 79: %d\n", answer);
 	return 0;
 }
