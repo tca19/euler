c2ac7072c1e3d0d438bc91d94b209875150acd00
tca19 <30314448+tca19@users.noreply.github.com>
2018-09-08 23:24:03 +0200

    optimize solution of problem 92 (memoization)

--- problem_076-100/problem_092.c
+++ problem_076-100/problem_092.c
@@ -3,6 +3,9 @@
 
 #define MAX 10000000
 
+int8_t arrive_at_89[MAX] = {0};
+
+/* square_digits: square each digit of n, return the sum */
 int32_t square_digits(int32_t n)
 {
 	int32_t res = 0;
@@ -16,14 +19,23 @@ int32_t square_digits(int32_t n)
 	return res;
 }
 
+/* chain_contains_89: compute a chain by recursively calling square_digits() on
+ * n. Return 1 if this chain contains the number 89 */
 int32_t chain_contains_89(int32_t n)
 {
+	/* the problem states that EVERY numbers will arrive at 1 or 89 */
 	while (n != 1 && n != 89)
+	{
+		/* look if we have already computed the chain for this number */
+		if (arrive_at_89[n])
+			return 1;
 		n = square_digits(n);
+	}
 
 	return n == 89;
 }
 
+/* compute number of times an integer ends with 89 when squaring its digits */
 int32_t main(void)
 {
 	int32_t i, count = 0;
@@ -31,7 +43,13 @@ int32_t main(void)
 	for (i = 1; i < MAX; ++i)
 	{
 		if (chain_contains_89(i))
+		{
+
+			/* mark it as seen so when we encounter it again in a
+			 * chain, we know it will ends at 89 */
+			arrive_at_89[i] = 1;
 			++count;
+		}
 	}
 
 	printf("Problem 92: %d\n", count);
