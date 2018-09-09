45da949c920bd8e459e117ec923a7c206e0c099b
tca19 <30314448+tca19@users.noreply.github.com>
2018-09-09 23:32:15 +0200

    add comment for solution 206

--- problem_201-225/problem_206.c
+++ problem_201-225/problem_206.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/* is_valid: return 1 if n has the form 1_2_3_4_5_6_7_8_9_0 (_ = 1 digit) */
 int32_t is_valid(int64_t n)
 {
 	int32_t i;
@@ -9,7 +10,7 @@ int32_t is_valid(int64_t n)
 
 	for (i = 9; i > 0; --i)
 	{
-		n /= 100;
+		n /= 100; /* only 1 out of 2 digits is interesting */
 		if (n%10 != i)
 			return 0;
 	}
@@ -17,6 +18,7 @@ int32_t is_valid(int64_t n)
 	return 1;
 }
 
+/* find the unique integer n such that t n*n is like 1_2_3_4_5_6_7_8_9_0 */
 int main(void)
 {
 	int64_t i;
