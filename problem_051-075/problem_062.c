53c0bdc1206555971a4996181e694ad13358b226
tca19 <30314448+tca19@users.noreply.github.com>
2018-09-12 22:53:23 +0200

    optimize solution 62 (hashmap for fast retrieval)

--- problem_051-075/problem_062.c
+++ problem_051-075/problem_062.c
@@ -4,14 +4,26 @@
 #include <string.h>
 
 #define MAXLEN  15
-#define MAXSIZE 10000
+#define HASHSIZE 10000
 
 struct permutation
 {
 	char* repr;
 	int8_t count;
 	int64_t smallest;
+	struct permutation *next;
 };
+
+
+/* hash: return the hash of string s */
+uint32_t hash(char *s)
+{
+	uint32_t hashval;
+
+	for (hashval = 0; *s != '\0'; ++s)
+		hashval = *s + 31 * hashval;
+	return hashval % HASHSIZE;
+}
 
 /* largest_permutation: return largest permutation formed with digits of n */
 char* largest_permutation(int64_t n)
@@ -41,44 +53,62 @@ char* largest_permutation(int64_t n)
 	return permutation;
 }
 
-/* insert: insert the largest permutation formed by n^3 or increment its count.
- * Return the corresponding index (the one inserted or the ones incremented). */
-int32_t insert(struct permutation *ar, int64_t n, int32_t *size)
+struct permutation* insert(struct permutation **hashtab, int64_t n)
 {
-	int32_t i;
-	char *permutation = largest_permutation(n*n*n);
+	char *representation;
+	uint32_t hashval;
+	struct permutation *p;
 
-	for (i = 0; i < *size; ++i, ++ar)
-		if (strcmp(ar->repr, permutation) == 0)
+	representation = largest_permutation(n*n*n);
+	hashval = hash(representation);
+
+	/* never added the representation into hashtab, so add it */
+	if (hashtab[hashval] == NULL)
+	{
+		p = (struct permutation *) malloc(sizeof *p);
+		p->repr = representation;
+		p->count = 1;
+		p->smallest = n*n*n;
+		p->next = NULL;
+		hashtab[hashval] = p;
+		return p;
+	}
+
+	/* hashval has already been seen, look if already in hashtab */
+	for (p = hashtab[hashval]; p != NULL; p = p->next)
+	{
+		if (strcmp(p->repr, representation) == 0) /* found */
 		{
-			ar->count++;
-			return i;
+			p->count++;
+			return p;
 		}
+	}
 
-	ar->repr = permutation;
-	ar->count = 1;
-	ar->smallest = n*n*n;
-	(*size)++; /* increase size because 1 more element in ar */
-	return *size-1;
+	/* not already in, add it at top of hashtab[hashval] */
+	p = (struct permutation *) malloc(sizeof *p);
+	p->repr = representation;
+	p->count = 1;
+	p->smallest = n*n*n;
+	p->next = hashtab[hashval];
+	hashtab[hashval] = p;
+	return p;
 }
 
 /* find the smallest cube for which exactly five permutations of its digits are
  * also cube. */
 int32_t main(void)
 {
-	int32_t index, size;
 	int64_t i;
-	struct permutation *list;
+	struct permutation *current, **hashtab;
 
-	size = 0;
-	list = calloc(MAXSIZE, sizeof *list);
+	hashtab = calloc(HASHSIZE, sizeof *hashtab);
 
-	for (i = 345; i < MAXSIZE; ++i)
+	for (i = 345; i < HASHSIZE; ++i)
 	{
-		index = insert(list, i, &size);
-		if (list[index].count == 5)
+		current = insert(hashtab, i);
+		if (current->count == 5)
 		{
-			printf("Problem 62: %ld\n", list[index].smallest);
+			printf("Problem 62: %ld\n", current->smallest);
 			break;
 		}
 	}
