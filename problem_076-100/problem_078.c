b9ccfc30693dc899be7e0f83b5ed3316c51abf22
tca19 <30314448+tca19@users.noreply.github.com>
2018-09-16 12:02:50 +0200

    optimize solution 78 with pentagonal numbers (x50)

--- problem_076-100/problem_078.c
+++ problem_076-100/problem_078.c
@@ -8,26 +8,47 @@
  * number of ways to partition the integer n) */
 int32_t main(void)
 {
-	int32_t i, j, partitions[LIM+1];
+	int32_t i, j, partitions[LIM] = {0}, pentagonal[LIM] = {0};
 
-	/* The integer j can be partitioned as j = i + (j-1) for i=1..j. The
-	 * integer j-i can also be partitioned in some ways, but since the
-	 * computations are done with increasing j, it has already been
-	 * computed. So partitions[j] = sum_{1}^j partitions[j-i] */
+	/* init the array with generalized pentagonal numbers. Stop once
+	 * pentagonal[i] > LIM because the formula to compute partitions[n],
+	 * uses partitions[n-k] where k is a pentagonal number and n=1..LIM, so
+	 * at one point, n-k will be negative and partitions[<0] = 0. */
+	for (i = 1, j = 0; j < LIM; ++i)
+	{
+		pentagonal[j++] = i * (3*i-1) / 2;
+		pentagonal[j++] = i * (3*i+1) / 2;
+		if (pentagonal[j] > LIM)
+			break;
+	}
+
+	/* use the recurrence formula to compute the number of partitions:
+	 *   partitions[n] = partitions[n-penta_1] + partitions[n-penta_2]
+	 *                 - partitions[n-penta_3] - partitions[n-penta_4]
+	 *                 + partitions[n-penta_5] + partitions[n-penta_6]...
+	 */
 	partitions[0] = 1;
 	for (i = 1; i < LIM; ++i)
-		for (j = i; j <= LIM; ++j)
+	{
+		for (j = 0; j < LIM; ++j)
 		{
-			partitions[j] += partitions[j-i];
-
-			/* need to find the first one which is divisible by
-			 * 10^6, so only keep the last 6 digits. Since we are
-			 * only adding, it is not a problem to do all operations
-			 * with % 10^6 */
-			partitions[j] %= MOD;
+			if ((i-pentagonal[j]) < 0)
+				break;
+			if ((j&2) == 0) /* simulate + + - - + + - - ... */
+				partitions[i] += partitions[i-pentagonal[j]];
+			else
+				partitions[i] -= partitions[i-pentagonal[j]];
 		}
 
-	for (i = 0; i <= LIM; ++i)
+		/* need to find the first one which is divisible by
+		 * 10^6, so only keep the last 6 digits. Since we are
+		 * only adding, it is not a problem to do all operations
+		 * with % 10^6 */
+		partitions[i] %= MOD;
+	}
+
+	/* find the first one divisible by 10^6 */
+	for (i = 0; i < LIM; ++i)
 		if (partitions[i] == 0) /* partitions[i] is already % MOD */
 		{
 			printf("Problem 78: %d\n", i);
